40a0dbaf590405b352940f310ec23f287f1a003a
ashfaqFullStack <ashfaqahmadfullstack@gmail.com>
2025-03-19 13:37:34 -0700

    full responsive home page

--- components/home/Card.tsx
+++ components/home/Card.tsx
@@ -1,19 +1,17 @@
 import pic from '@/data/mongodbicon.svg'
 import Image from 'next/image'
 import MongoDB from '@/data/mongodb.svg'
+import { BsPlusLg } from "react-icons/bs";
 
 function Card() {
     return (
         <div className='mt-[120px] md:mt-[130px] lg:mt-[148px]' >
             <div className=' text-[black] md:text-[41.9px] text-[39px] z-50 md:text-start text-center font-semibold md:absolute '>Reliability you can count on.</div>
-            {/* <div className='grid grid-cols-12    font-bold text-[black]    relative  top-0  lg:border-r border-[#e3dcdc] mt-[40px] md:mt-[101px]  '>
-                <div className=' col-span-4    lg:border-r md:py-[48px] md:px-[48px] border-[#e3dcdc]'></div>
-                <div className=' col-span-4  lg:border-r border-[#e3dcdc]' ></div>
-            </div> */}
 
-            <div className=' grid grid-cols-12 items-center text-[black] md:text-start text-center  '>
+
+            <div className=' grid grid-cols-12 items-center text-[black] md:text-start text-center '>
                 {/* first row of content  */}
-                <div className=' md:col-span-4 col-span-12  lg:border-r border-[#e3dcdc] md:pr-[48px]  md:py-[148px]'>
+                <div className=' md:col-span-4 col-span-12  lg:border-r border-[#e3dcdc] md:pr-[48px]  md:py-[164px] '>
                     <div className='max-w-[70%] md:max-w-full mx-auto md:mx-0 mt-[58px] md:mt-0 flex items-center justify-center md:justify-start text-[#666666] text-center md:text-start gap-[8px]'>
                         <Image src={MongoDB} alt="" />
                         <div className=' md:text-[19px] font-normal'>Start  Your project</div>
@@ -27,7 +25,7 @@ function Card() {
 
                     </div>
                 </div>
-                <div className=' md:col-span-4 col-span-12   lg:border-r border-[#e3dcdc] px-[25px] lg:px-[48px]  md:py-[148px]'>
+                <div className=' md:col-span-4 col-span-12   lg:border-r border-[#e3dcdc] px-[25px] lg:px-[48px]  md:py-[149px]'>
                     <div className='max-w-[70%] md:max-w-full mx-auto md:mx-0 mt-[58px] md:mt-0 flex items-center justify-center md:justify-start text-[#666666] text-center md:text-start gap-[8px]'>
                         <Image src={MongoDB} alt="" />
                         <div className=' md:text-[19px] font-normal'>Start  Your project</div>
@@ -41,14 +39,18 @@ function Card() {
 
                     </div>
                 </div>
-                <div className=' md:col-span-4 col-span-12  max-h-[40%] flex items-center border-l   md:border-l-0 border-y border-[#e3dcdc] md:px-[48px] py-[10%] md:mx-0 mx-[128px]'>
-                    <div className=' ml-[30px] md:ml-0 border-l-2 py-[4%] border-[#c9c9c9] bg-[#17171700]  '>
-                        <div className=' text-[42px]  pl-4  text-[#FAFAFA]'>99.99% Uptime</div>
+                <div className='relative md:col-span-4 col-span-12 md:max-h-[40%] flex items-center border-l md:border-l-0 border-t border-b border-[#e3dcdc] my-[6%] md:my-0 py-[4%] md:py-[10%] md:px-[24px] md:mx-0 '>
+
+                    <BsPlusLg className='absolute top-0 left-0 translate-x-[-50%] translate-y-[-50%] text-[#A8A8A8] text-[32px]' />
+
+                    <div className=' ml-[24px] md:ml-0 border-l-2 py-[1%] md:py-[4%] border-[#c9c9c9] bg-[#17171700]'>
+                        <div className='flex text-[35px] md:text-[42px] w-full pl-[16px] font-semibold text-[#FAFAFA]'>99.99% Uptime</div>
                     </div>
+                    <BsPlusLg className='absolute bottom-0 right-0  translate-x-[50%] translate-y-[50%] text-[#A8A8A8] text-[32px]' />
                 </div>
 
                 {/* second row of content */}
-                <div className=' md:col-span-4 col-span-12  lg:border-r border-[#e3dcdc] md:pr-[48px]  md:py-[48px]'>
+                <div className=' md:col-span-4 col-span-12 lg:border-r border-[#e3dcdc] md:pr-[48px] md:pb-[84px] '>
                     <div className='max-w-[70%] md:max-w-full mx-auto md:mx-0 mt-[18px] md:mt-0  flex items-center justify-center md:justify-start text-[#666666] text-center md:text-start gap-[8px]'>
                         <Image src={MongoDB} alt="" />
                         <div className=' md:text-[19px] font-normal'>Start  Your project</div>
@@ -62,8 +64,8 @@ function Card() {
 
                     </div>
                 </div>
-                <div className=' md:col-span-4 col-span-12   lg:border-r border-[#e3dcdc] px-[25px] lg:px-[48px]  md:py-[48px]'>
-                    <div className='max-w-[70%] md:max-w-full mx-auto md:mx-0 mt-[58px] md:mt-0  flex items-center justify-center md:justify-start text-[#666666] text-center md:text-start gap-[8px]'>
+                <div className=' md:col-span-4 col-span-12 lg:border-r border-[#e3dcdc] px-[25px] lg:px-[48px] md:pb-[84px] '>
+                    <div className='max-w-[70%] md:max-w-full mx-auto md:mx-0 mt-[18px] md:mt-0  flex items-center justify-center md:justify-start text-[#666666] text-center md:text-start gap-[8px]'>
                         <Image src={MongoDB} alt="" />
                         <div className=' md:text-[19px] font-normal'>Start  Your project</div>
                     </div>
@@ -76,8 +78,8 @@ function Card() {
 
                     </div>
                 </div>
-                <div className=' md:col-span-4 col-span-12   lg:border-r border-[#e3dcdc] px-[25px] lg:px-[48px]  md:py-[48px]'>
-                    <div className='max-w-[70%] md:max-w-full mx-auto md:mx-0 mt-[58px] md:mt-0  flex items-center justify-center md:justify-start text-[#666666] text-center md:text-start gap-[8px]'>
+                <div className=' md:col-span-4 col-span-12  px-[25px] lg:px-[48px]   md:pb-[84px]   '>
+                    <div className='max-w-[70%] md:max-w-full mx-auto md:mx-0 mt-[18px] md:mt-0  flex items-center justify-center md:justify-start text-[#666666] text-center md:text-start gap-[8px]'>
                         <Image src={MongoDB} alt="" />
                         <div className=' md:text-[19px] font-normal'>Start  Your project</div>
                     </div>
@@ -90,6 +92,7 @@ function Card() {
 
                     </div>
                 </div>
+
             </div>
         </div>
     )
--- components/home/LastSection.tsx
+++ components/home/LastSection.tsx
@@ -13,178 +13,137 @@ import secondman5 from "@/data/secondman5.svg"
 import thirdwoman1 from "@/data/thirdwoman1.svg"
 import thirdwoman2 from "@/data/thirdwoman2.svg"
 import thirdwoman3 from "@/data/thirdwoman3.svg"
-
-
-
-
-
-
-
-
-
-
-
 import Image from "next/image"
+import { BsPlusLg } from "react-icons/bs"
+
 function LastSection() {
     return (
-        <div className=" text-[black]">
-            <div className=" grid grid-cols-12 md:mx-[228px] mt-[47px] mb-10">
-                <div className=" md:col-span-6 col-span-12 md:mx-0   mx-4 pr-[48px] py-[48px] border-b border-[#00000014]">
-                    <div className="flex items-center  md:justify-start justify-center gap-[8px]">
-                        <Image src={circle} alt="circle" className=" " />
+        <div className="grid grid-cols-12 mt-[60px] md:mt-0 relative border-b border-[#e3dcdc]" >
+            <BsPlusLg className='absolute top-0 left-0 translate-x-[-50%] translate-y-[-50%] text-[#A8A8A8] text-[32px]' />
 
-                        <div className=" text-[15px]  font-normal">Instant Rollbacks</div>
-                    </div>
-
-                    <div className=" text-[23px] font-bold md:text-start text-center">Go ahead, deploy on Friday. <span className=" text-[#666666] font-normal">Instantly</span> </div>
-                    <div className=" text-[23px] text-[#666666] font-[500] md:text-start text-center">rollback to a working deployment.</div>
-                    {/* input >>>>(1) */}
-                    <div className="flex justify-between items-center  gap-[24px]  lg:w-[75%]       mt-[32px] bg-[#FFFFFF]  py-[13px]    px-[13px] rounded-[12px]  border-1 border-[#00000014]">
-                        <div>
-
-                            <div className="flex   justify-between" >
-                                <div className=" text-[#666666]  font-normal">vercel-site/ <span className=" text-[black]">jvjb4ynna</span> </div>
-                                <div>1d agc</div>
-                            </div>
-                            <div className="flex items-center gap-[8px] ">
-                                <Image src={range} alt="circle" className=" " />
-                                <div className="text-[13px] font-normal">ba5f55f</div>
-                                <div className="text-[14px] font-normal">Update bento box design</div>
-                            </div>
-
-                        </div>
-                        <div className="  font-[500] text-[11px]">90</div>
-                    </div>
-
-                    {/* image>>>> (1) */}
-                    <div className="   md:px-[230px] mt-[55px] md:block hidden">
-                        <div className=" border rounded-full w-[30px] h-[30px]  flex justify-center  !relative  ">
-                            <Image src={circle} alt="circle" className=" " />
-                        </div>
-                    </div>
-                    {/* image>>>> (2) */}
-                    <div className="  md:block hidden">
-                        <Image src={errow} alt="circle" className=" absolute xlg:top-11/4  lg:top-28/12 md::top-28/12  xlg:mt-[70px] :mt-[70px]   lg:mt-[3rem]  md:mt-[30rem] ml-41" />
-                    </div>
-                    {/* input >>>>(2) */}
-
-                    <div className="flex justify-between items-center  gap-[24px] md:w-[75%] xlg:mt-[53px]  lg:mt-[65px] mt-5 bg-[#FFFFFF]  py-[13px] px-[13px] rounded-[12px]  border-1 border-[#00000014]   md:ml-[110px] ">
-                        <div>
-
-                            <div className="flex   justify-between" >
-                                <div className=" text-[#666666]  font-normal">vercel-site/ <span className=" text-[black]">jvjb4ynna</span> </div>
-                                <div>1d agc</div>
-                            </div>
-                            <div className="flex items-center gap-[8px] ">
-                                <Image src={range} alt="circle" className=" " />
-                                <div className="text-[13px] font-normal">ba5f55f</div>
-                                <div className="text-[14px] font-normal">Update bento box design</div>
-                            </div>
-
-                        </div>
-                        <div className="  font-[500] text-[11px]">90</div>
-                    </div>
-
-
+            <div className="col-span-12 md:col-span-6 relative md:border-r border-[#e3dcdc] p-[24px] md:p-[48px]" >
+                <div className="flex items-center gap-[8px]" >
+                    <Image src={circle} alt="Loading..." />
+                    Instant Rollbacks
                 </div>
 
-                <div className=" md:col-span-6 col-span-12 border-l   md:px-[48px] px-4 py-[48px] border-b border-[#00000014] ">
-
-                    <div className="flex items-center  md:justify-start justify-center gap-[8px]">
-                        <div className="flex">
-                            <Image src={Conformance} alt="circle" className=" " />
-                            <Image src={Conformance} alt="circle" className=" " />
-                            <Image src={Conformance} alt="circle" className=" " />
-
-                        </div>
-                        <div className=" text-[15px]  font-normal">Instant Rollbacks</div>
-                    </div>
-
-                    <div className=" text-[23px] font-bold md:text-start text-center">Move fast, don’t break things. <span className=" text-[#666666] font-normal">Keep</span> </div>
-                    <div className=" text-[23px] text-[#666666] font-[500] md:text-start text-center">quality high while maintaining velocity .</div>
-                    <div className=" text-[23px] text-[#666666] font-[500] md:text-start text-center">with Enterprise Monorepos.</div>
-
-                    <div className="  md:flex border border-[#00000014] rounded-[12px] mt-3">
-                        <div className="bg-[blue  md:w-[50%]  ">
-                            <div className=" text-[20px] mx-[16px] mt-[16px] font-[500] md:text-start text-center ">Conformance</div>
-
-                            <div className="  flex  justify-between bg-[#FAFAFA] rounded-[6px] mx-[16px] px-[12px] py-[20px] mt-[16px]">
-                                <div className="  text-[11px] font-normal">Excellent</div>
-                                <div className=" text-[15px]  font-bold">9.5</div>
-                            </div>
-
-                            <div className="  flex  justify-between bg-[#FAFAFA] rounded-[6px] mx-[16px] px-[12px] py-[20px] mt-[16px]">
-                                <div className="  text-[11px] font-normal">Excellent</div>
-                                <div className=" text-[15px]  font-bold">9.5</div>
-                            </div>
-
-                            <div className="  flex  justify-between bg-[#FAFAFA] rounded-[6px] mx-[16px] px-[12px] py-[20px] mt-[16px]">
-                                <div className="  text-[11px] font-normal">Excellent</div>
-                                <div className=" text-[15px]  font-bold">9.5</div>
-                            </div>
-
-                        </div>
-                        <div className="bg-[red md:w-[50%] border-l  border-[#00000014]">
-                            <div className=" text-[20px] mx-[16px] mt-[16px] font-[500] md:text-start text-center ">Conformance</div>
-
-                            <div className="  flex  justify-between bg-[#FAFAFA] rounded-[6px] mx-[16px] px-[12px] py-[20px] mt-[16px]">
-
-                                <div className="  text-[11px] font-normal  text-[#666666]">@vercel/design</div>
-                                <div className=" flex ">
-                                    <Image src={firstman} alt="circle" className=" " />
-                                    <Image src={firstwoman} alt="circle" className=" " />
-                                    <Image src={firstgb} alt="circle" className=" " />
-                                </div>
-
-                            </div>
-
-
-                            <div className="  flex  justify-between bg-[#FAFAFA] rounded-[6px] mx-[16px] px-[12px] py-[20px] mt-[16px]">
-
-                                <div className="  text-[11px] font-normal  text-[#666666]">@vercel/design</div>
-                                <div className=" flex  ">
-                                    <Image src={secondman1} alt="circle" className=" " />
-                                    <Image src={secondman2} alt="circle" className=" " />
-                                    <Image src={secondman3} alt="circle" className=" " />
-                                    <Image src={secondman4} alt="circle" className=" " />
-                                    <Image src={secondman5} alt="circle" className=" " />
-
-                                </div>
-
-                            </div>
-
-
-
-                            <div className="  flex  justify-between bg-[#FAFAFA] rounded-[6px] mx-[16px] px-[12px] py-[20px] mt-[16px]">
-
-                                <div className="  text-[11px] font-normal  text-[#666666]">@vercel/design</div>
-                                <div className=" flex ">
-                                    <Image src={thirdwoman1} alt="circle" className=" " />
-                                    <Image src={thirdwoman2} alt="circle" className=" " />
-                                    <Image src={thirdwoman3} alt="circle" className=" " />
-
-
-                                </div>
-
-                            </div>
-
-                        </div>
-
-                    </div>
-
-
-
-
-
-
-
-
-
-
+                <div className="text-[18px] text-center md:text-start md:text-[20px] lg:text-[24px] mt-[16px]" >
+                    <span className="font-semibold" >Go ahead, deploy on Friday.</span>Instantly rollback to a working deployment.
                 </div>
 
+                <div className="flex w-fit gap-[26px] mt-[32px] items-center rounded-[12px] shadow-md  font-normal p-[13px] border border-[#e3dcdc]" >
+                    {/* box first row  */}
+                    <div>
+                        <div className="flex justify-between items-center text-[10px] md:text-[13px] ">
+                            <div>
+                                <span className="text-gray-300" >vercel-site/</span>jvjb4ynna
+                            </div>
+                            <div className="text-gray-300" >
+                                1d ago
+                            </div>
+                        </div>
+                        {/* box second row */}
+                        <div className="flex items-center text-[8px] md:text-[13px] justify-between gap-[8px]" >
+                            <Image src={range} alt="Range" />
+                            <span> ba5f55f </span>
+                            <span> Update bento box design </span>
+                        </div>
+                    </div>
+
+                    <div className="text-[12px] md:text-[13px]"  >90</div>
+                </div>
+
+                {/* Arrow */}
+
+                <div className="relative h-[20vh]" >
+                    <Image src={circle} alt="Errow" className="absolute border border-[#e3dcdc] rounded-full p-[5px] w-[26px]  top-[44%] left-[50%] translate-x-[-50%] translate-y-[-50%] " />
+                    <Image src={errow} alt="Errow" className="absolute top-[46%] left-[50%] translate-x-[-50%] translate-y-[-50%] " />
+                </div>
+
+                {/* box */}
+
+                <div className="flex !mt-[-16px] ml-auto w-fit gap-[26px] items-center rounded-[12px] shadow-md font-normal p-[13px] border-2 border-dashed border-[#e3dcdc]" >
+                    {/* box first row  */}
+                    <div>
+                        <div className="flex justify-between  items-center text-[10px] md:text-[13px]  ">
+                            <div>
+                                <span className="text-gray-300" >vercel-site/</span>jvjb4ynna
+                            </div>
+                            <div className="text-gray-300" >
+                                1d ago
+                            </div>
+                        </div>
+                        {/* box second row */}
+                        <div className="flex  items-center text-[8px] md:text-[13px] justify-between gap-[8px]" >
+                            <Image src={range} alt="Range" />
+                            <span> ba5f55f </span>
+                            <span> Update bento box design </span>
+                        </div>
+                    </div>
+
+                    <div className="text-[12px] md:text-[13px]" >90</div>
+                </div>
             </div>
-            <div className=" bg-white h-3.5"></div>
+
+            {/* Conformance */}
+
+            <div className=" col-span-12 md:col-span-6 px-[24px] md:px-[48px]" >
+                <div className="flex items-center gap-[8px]" >
+                    <Image src={circle} alt="Loading..." />
+                    Instant Rollbacks
+                </div>
+
+                <div className="text-[18px] text-center md:text-start md:text-[20px] lg:text-[24px] mt-[16px]" >
+                    <span className="font-semibold" >Move fast, don&apos;t break things.</span>Keep quality high while maintaining velocity with Enterprise Monorepos.
+                </div>
+
+                <div className="grid grid-cols-12 mt-[32px] rounded-[12px]  border border-[#e3dcdc]" >
+                    <div className="col-span-12 md:col-span-6 p-[16px] md:border-r border-[#e3dcdc]" >
+                        <div className="text-[14px] font-medium" >Conformance</div>
+
+                        <div className="bg-[#FAFAFA] py-[20px] mt-[16px] text-[11px] rounded-[6px] px-[12px] flex items-center justify-between" >
+                            <div className="text-[#666666]" >Excellent</div>
+                            <div className="font-semibold" >9.5</div>
+                        </div>
+                        <div className="bg-[#FAFAFA] py-[20px] mt-[16px] text-[11px] rounded-[6px] px-[12px] flex items-center justify-between" >
+                            <div className="text-[#666666]" >Total Issues</div>
+                            <div className="font-semibold" >34</div>
+                        </div>
+                        <div className="bg-[#FAFAFA] py-[20px] mt-[16px] text-[11px] rounded-[6px] px-[12px] flex items-center justify-between" >
+                            <div className="text-[#666666]" >Major Issues</div>
+                            <div className="font-semibold" >12</div>
+                        </div>
+                    </div>
+                    <div className="col-span-12 md:col-span-6 p-[16px] border-r border-[#e3dcdc]" >
+                        <div className="text-[14px] font-medium" >Code Owners</div>
+
+                        <div className="bg-[#FAFAFA] flex-wrap py-[20px] mt-[16px] text-[11px] rounded-[6px] px-[12px] flex items-center justify-between" >
+                            <div className="text-[#666666]" >@vercel/design</div>
+                            <div className="flex" >
+                                <Image src={firstwoman} alt="Range" className="mr-[-10px]" />
+                                <Image src={firstwoman} alt="Range" className="mr-[-10px]" />
+                                <Image src={firstwoman} alt="Range" />
+                            </div>
+                        </div>
+                        <div className="bg-[#FAFAFA] flex-wrap py-[20px] mt-[16px] text-[11px] rounded-[6px] px-[12px] flex items-center justify-between" >
+                            <div className="text-[#666666]" >@vercel/eng</div>
+                            <div className="flex" >
+                                <Image src={firstwoman} alt="Range" className="mr-[-10px]" />
+                                <Image src={firstwoman} alt="Range" className="mr-[-10px]" />
+                                <Image src={firstwoman} alt="Range" />
+                            </div>
+                        </div>
+                        <div className="bg-[#FAFAFA] flex-wrap py-[20px] mt-[16px] text-[11px] rounded-[6px] px-[12px] flex items-center justify-between" >
+                            <div className="text-[#666666]" >@vercel/marketing</div>
+                            <div className="flex" >
+                                <Image src={firstwoman} alt="Range" className="mr-[-10px]" />
+                                <Image src={firstwoman} alt="Range" className="mr-[-10px]" />
+                                <Image src={firstwoman} alt="Range" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
