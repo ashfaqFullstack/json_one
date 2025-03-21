4adeae07860c7d218da33329c982224401e857e4
ashfaqFullstack <ashfaqahmadfullstack@gmail.com>
2025-03-22 00:17:29 +0500

    Login and SignUp page full responsive

---
+++ components/home/Verifyemail.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import downerrow from '@/data/downerrow.svg'
+import geo from '@/data/geo.svg'
+function Verifyemail() {
+  return (
+    <div>Verifyemail</div>
+  )
+}
+
+export default Verifyemail
